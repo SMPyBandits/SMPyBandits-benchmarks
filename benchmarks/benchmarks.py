be45d48aa365be3ae9803d385d7e205f2795fa0a
Lilian Besson <lilian.besson@crans.org>
2019-10-24 17:13:17 +0200

    Update benchmark.

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -7,6 +7,8 @@ class TimeSuite:
     An example benchmark that times the performance of various kinds
     of iterating over dictionaries in Python.
     """
+    params = [100, 500, 1000, 5000]
+
     def setup(self):
         self.d = {}
         for x in range(500):
@@ -16,21 +18,17 @@ class TimeSuite:
         for key in self.d.keys():
             pass
 
-    def time_iterkeys(self):
-        for key in self.d.iterkeys():
-            pass
-
     def time_range(self):
         d = self.d
         for key in range(500):
             x = d[key]
 
-    def time_xrange(self):
-        d = self.d
-        for key in xrange(500):
-            x = d[key]
-
 
 class MemSuite:
+    params = [100, 500, 1000, 5000]
+
+    def setup(self, n):
+        self.n = n
+
     def mem_list(self):
-        return [0] * 256
+        return [0] * self.n

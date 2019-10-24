0b110a083f14f57086760bc5f052af97816e9ded
Lilian Besson <lilian.besson@crans.org>
2019-10-24 17:53:56 +0200

    Trying...

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -22,9 +22,9 @@ algorithm_map = {
     "BayesUCB": Policies.BayesUCB,
 }
 
-values_algorithm = tuple(algorithm_map.keys()),
-values_nbArms = (2, 3, 4) #, 8, 12, 16, 24, 32, 48, 64)
-values_horizon = (100),  #, 250, 500, 750, 1000, 2000),
+values_algorithm = list(algorithm_map.keys()),
+values_nbArms = [2, 3, 4] #, 8, 12, 16, 24, 32, 48, 64]
+values_horizon = [100, 250],  #, 250, 500, 750, 1000, 2000],
 
 
 class SMPyBanditsSuite:
@@ -36,21 +36,21 @@ class SMPyBanditsSuite:
         values_nbArms,
         values_horizon,
     )
-    params_names = (
+    param_names = (
         "algorithm",
         "nbArms",
         "horizon",
     )
 
     def setup(self, algorithm, nbArms, horizon):
-        self.algorithm = algorithm_map[algorithm]
-        self.nbArms = nbArms
-        self.horizon = horizon
         configuration = {
             'arm_type': Arms.Bernoulli,
             'params':   tuple(np.arange(0.1, 0.9, nbArms)),
         }
         self.MAB = SMPyBandits.Environment.MAB(configuration)
+        self.algorithm = algorithm_map[algorithm]
+        self.nbArms = nbArms
+        self.horizon = horizon
 
     def mem_createAlgorithm(self, algorithm, nbArms, horizon):
         alg = self.algorithm(self.nbArms)
@@ -58,11 +58,26 @@ class SMPyBanditsSuite:
         return alg
 
     def time_choice(self, algorithm, nbArms, horizon):
-        alg = self.algorithm(self.nbArms)
+        alg = self.algorithm(nbArms)
         alg.startGame()
         for t in range(horizon):
             arm = alg.choice()
-            # reward = 0  # FIXME
+            reward = self.MAB.draw(arm)
+            # alg.getReward(arm, reward)
+
+    def time_choice_and_getReward(self, algorithm, nbArms, horizon):
+        alg = self.algorithm(nbArms)
+        alg.startGame()
+        for t in range(horizon):
+            arm = alg.choice()
+            reward = self.MAB.draw(arm)
+            alg.getReward(arm, reward)
+
+    def time_getReward(self, algorithm, nbArms, horizon):
+        alg = self.algorithm(nbArms)
+        alg.startGame()
+        for t in range(horizon):
+            arm = np.random.randint(nbArms)
             reward = self.MAB.draw(arm)
             alg.getReward(arm, reward)
 

eee9d92d4b711a569bb8cf6afd0728587418bc58
Lilian Besson <lilian.besson@crans.org>
2019-10-25 16:58:25 +0200

    Improved README, added first screenshots

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8; mode: python -*-
 """
-setup.py script for the ParcourSup.py project (https://github.com/Naereen/ParcourSup.py)
+setup.py script for the SMPyBandits-benchmarks project (https://github.com/Naereen/SMPyBandits-benchmarks)
 
 References:
 - https://packaging.python.org/en/latest/distributing/#setup-py
@@ -17,20 +17,29 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-long_description = "test-of-airspeed-velocity"
+long_description = """
+Airspeed Velocity benchmarks for SMPyBandits
+https://github.com/Naereen/SMPyBandits-benchmarks
+
+This repository contains code (and soon, also results) of benchmarks for the SMPyBandits python package, using the airspeed velocity tool.
+
+This project is written by Lilian Besson's, written in Python (2 or 3), to test the quality of SMPyBandits, my open-source Python package for numerical simulations on slot_machine single-player and multi-players Multi-Armed Bandits (MAB) algorithms.
+
+A complete Sphinx-generated documentation for SMPyBandits is on SMPyBandits.GitHub.io.
+"""
 
 
 version = "0.0.1"
 
 
-setup(name="test-of-airspeed-velocity",
+setup(name="SMPyBandits-benchmarks",
     version=version,
-    description="test-of-airspeed-velocity",
+    description="SMPyBandits-benchmarks",
     long_description=long_description,
     author="Lilian Besson",
     author_email="naereen AT crans DOT org".replace(" AT ", "@").replace(" DOT ", "."),
-    url="https://github.com/Naereen/test-of-airspeed-velocity",
-    download_url="https://github.com/Naereen/test-of-airspeed-velocityreleases/",
+    url="https://github.com/Naereen/SMPyBandits-benchmarks",
+    download_url="https://github.com/Naereen/SMPyBandits-benchmarksreleases/",
     license="MIT",
     platforms=["GNU/Linux"],
     # https://pypi.python.org/pypi?%3Aaction=list_classifiers
